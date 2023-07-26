10358e2e7b8b7f76e4bb35b0a4aeb8de6458ecf5
Pâmela Borges <pamela_borges18@hotmail.com>
2023-07-26 11:55:48 -0300

    Segunda parte do CRUD

---
+++ src/main/java/com/udemy/curso/primeiro_projeto/services/ProdutoService.java
@@ -0,0 +1,78 @@
+package com.udemy.curso.primeiro_projeto.services;
+
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udemy.curso.primeiro_projeto.model.Produto;
+import com.udemy.curso.primeiro_projeto.repository.ProdutoRepository;
+
+@Service
+public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    /**
+     * Metodo para retornar uma lista de produtos
+     * 
+     * @return Lista de produtos
+     */
+
+    public List<Produto> obterTodos() {
+
+        return produtoRepository.obterTodos();
+    }
+
+    /**
+     * Metodo que retorna o produto pelo seu id
+     * 
+     * @param id do produto que será localizado.
+     * @return retorna o produto caso seja encontrado
+     */
+
+    public Optional<Produto> obterPorId(Integer id) {
+
+        return produtoRepository.obterPorId(id);
+    }
+
+    /**
+     * Metodo para adicionar produto na lista
+     * 
+     * @param produto que sera adicionado
+     * @return retorna o produto que será adicionado na lista
+     */
+
+    public Produto adicionar(Produto produto) {
+
+        return produtoRepository.adicionar(produto);
+
+    }
+
+    /**
+     * Metodo para deletar produto por id
+     * 
+     * @param id do prduto a ser deletado
+     */
+    public void deletar(Integer id) {
+
+        produtoRepository.deletar(id);
+    }
+
+    /**
+     * Metodo para atualizar produto na lista
+     * 
+     * @param produto que sera atualizado
+     * @return retorna o produto apos atualizar a lista
+     */
+    public Produto atualizar(Integer id, Produto produto) {
+
+        produto.setId(id);
+        return produtoRepository.atualizar(produto);
+
+    }
+
+}
